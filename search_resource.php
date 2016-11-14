592520ad87a6e46df8da152a49997f0ec5232785
Yue Wang <wangmelody@gatech.edu>
2016-11-14 13:01:32 -0500

    create search_resource

---
+++ search_resource.php
@@ -0,0 +1,58 @@
+<?php
+	include 'header.php';
+	$con = mysqli_connect($mysql_info['host'],$mysql_info['user'],$mysql_info['pwd'],$mysql_info['dbname']);
+	$username=$_SESSION[session_id()]['Username'];
+	$query_get_incident="select IncidentID, IncidentDescription from incident where OwnerUsername='".$username."';";
+	$result_get_incident=mysqli_query($con,$query_get_incident);
+	if (mysqli_num_rows($result_get_incident)!=0) {
+		$incident_exist=true;
+		$user_incident_array=array();
+		while($row_get_incident=mysqli_fetch_array($result_get_incident,MYSQLI_ASSOC)){
+			$user_incident_array[$row_get_incident['IncidentID']]=$row_get_incident['IncidentDescription'];}
+	} else {
+		$incident_exist=false;
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<h4>Search Resource</h4>
+</head>
+<body>
+<form>
+	<table>
+		<!-- row of keyword-->
+		<tr> <td>Keyword</td><td><input type="text" name="Keyword"></td></tr>
+		<!-- row of esf-->
+		<tr><td>ESF</td><td> <select name="esf_search">
+				<?php 
+					foreach ($_SESSION['esf'] as $key => $value) {
+						echo '<option value='.$key.'>#'.$key.":".$value.'</option>';
+						}
+				?>
+					</select>
+			
+						</td></tr>
+
+		<!-- row of location -->
+		<tr><td>Location</td><td>Within <input type="text" name="incident_radius"> Kilometers of incident</td></tr>
+		<tr><td> Incident </td> <td> <select name='corresponding_incident'>
+								<option value='none'>No inicident </option>;
+								<?php
+								if ($incident_exist) {
+									foreach ($user_incident_array as $key => $value) {
+										echo '<option value='.$key.'>#'.$key.$value.'</option>';
+									}
+								}
+								?> </select>
+			
+		</td></tr>
+
+	</table>
+</form>
+</body>
+</html>
+
+
+
+
